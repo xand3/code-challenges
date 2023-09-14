67a9944033a966c5aa1575d697f061d69ad21c19
Alexandre <xande.bastos321@gmail.com>
2023-09-14 18:56:36 -0300

    :notebook: Java code challenges

--- Java/CharProblem.java
+++ Java/CharProblem.java
@@ -3,7 +3,6 @@
 public class CharProblem {
     public static int howOld(final String herOld) {
         String phrase = herOld.split(" ")[0];
-        int age = Integer.parseInt(phrase);
-        return age;
+        return Integer.parseInt(phrase);
     }
 }
