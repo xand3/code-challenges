9a64ea4fa7378c0bfca773adbefe593f9e40868f
Alexandre <xande.bastos321@gmail.com>
2023-09-19 17:05:17 -0300

    :notebook: TypeScript code challenges

---
+++ TypeScript/order-please.ts
@@ -0,0 +1,5 @@
+// Your order, please
+
+export function order(words: string): string {
+  return words.split(" ").sort((a, b) => Number(a.replace(/[a-zA-Z]/g, '')) - Number(b.replace(/[a-zA-Z]/g, ''))).join(" ")
+}
