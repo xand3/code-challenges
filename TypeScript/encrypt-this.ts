ef11b340eb1ac4e10840268cfc36a8e2741c653c
Alexandre <xande.bastos321@gmail.com>
2023-09-20 22:16:52 -0300

    :notebook: TypeScript code challenges

--- TypeScript/encrypt-this.ts
+++ TypeScript/encrypt-this.ts
@@ -1,5 +1,17 @@
+// Encrypt this!
+
 export const encryptThis = (str: string): string => {
   return str.split(" ").map(word => {
-    
+    if (word.length === 0) {
+      return "";
+    }
+    const firstLetter: number = word[0].charCodeAt(0);
+    if(word.length === 1) {
+      return firstLetter.toString();
+    } else if(word.length === 2) {
+      return firstLetter + word[1];
+    } else {
+      return firstLetter + word[word.length - 1] + word.slice(2, word.length - 1) + word[1];
+    }
   }).join(" ")
 }
