1596f3e8c812fb5c7f48856fe3a73ef53fa198af
Alexandre <xande.bastos321@gmail.com>
2023-09-22 16:03:10 -0300

    :notebook: TypeScript code challenges

--- TypeScript/build-tower.ts
+++ TypeScript/build-tower.ts
@@ -1,6 +1,7 @@
 export const towerBuilder = (nFloors: number): string[] => {
   const tower: string[] = [];
   const width: number = nFloors * 2 - 1;
+  
   const middle: number = Math.floor(width / 2);
   for (let i = 0; i < nFloors; i++) {
     const floor: string[] = Array(width).fill(' ');
